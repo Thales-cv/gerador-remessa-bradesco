8eb0e63e9625b3621b2c6e8a629079c522acd285
Thales C. Veiga <thales.cveiga@gmail.com>
2026-01-13 16:21:13 -0300

    op

--- src/cnab_definitions.py
+++ src/cnab_definitions.py
@@ -59,7 +59,8 @@ HEADER_LOTE = {
     "cep": (213, 217, PadType.ZERO_LEFT, ""),
     "complemento_cep": (218, 220, PadType.SPACE_RIGHT, ""),
     "estado": (221, 222, PadType.SPACE_RIGHT, ""),
-    "cnab_vazio_2": (223, 230, PadType.SPACE_RIGHT, ""), 
+    "forma_pagamento_servico": (223, 224, PadType.ZERO_LEFT, "01"), # NEW: Force 01
+    "cnab_vazio_2": (225, 230, PadType.SPACE_RIGHT, ""), 
     "ocorrencias": (231, 240, PadType.SPACE_RIGHT, "")
 }
 
@@ -93,6 +94,40 @@ SEGMENTO_A = {
     "cod_finalidade_doc": (234, 235, PadType.SPACE_RIGHT, ""),
     "cod_finalidade_ted": (236, 240, PadType.SPACE_RIGHT, "") 
 }
+
+# Special Layout for Pix in Segment A:
+# Cols 227-229 (Reservado Febraban) must be blank.
+# Cols 231-240 (Reservado Banco) must be blank.
+SEGMENTO_A_PIX = {
+    "banco": (1, 3, PadType.ZERO_LEFT, "237"),
+    "lote": (4, 7, PadType.ZERO_LEFT, ""),
+    "registro": (8, 8, PadType.ZERO_LEFT, "3"),
+    "n_registro": (9, 13, PadType.ZERO_LEFT, ""), 
+    "segmento": (14, 14, PadType.SPACE_RIGHT, "A"),
+    "tipo_movimento": (15, 15, PadType.ZERO_LEFT, "0"), 
+    "codigo_instrucao": (16, 17, PadType.ZERO_LEFT, "00"),
+    "camara": (18, 20, PadType.ZERO_LEFT, ""), 
+    "banco_favorecido": (21, 23, PadType.ZERO_LEFT, ""),
+    "agencia_favorecido": (24, 28, PadType.ZERO_LEFT, ""),
+    "agencia_dv_favorecido": (29, 29, PadType.SPACE_RIGHT, ""),
+    "conta_favorecido": (30, 41, PadType.ZERO_LEFT, ""),
+    "conta_dv_favorecido": (42, 42, PadType.SPACE_RIGHT, ""),
+    "dv_ag_conta_favorecido": (43, 43, PadType.SPACE_RIGHT, ""),
+    "nome_favorecido": (44, 73, PadType.SPACE_RIGHT, ""),
+    "n_doc_empresa": (74, 93, PadType.SPACE_RIGHT, ""), 
+    "data_pagamento": (94, 101, PadType.ZERO_LEFT, ""),
+    "tipo_moeda": (102, 104, PadType.SPACE_RIGHT, "BRL"),
+    "quantidade_moeda": (105, 119, PadType.ZERO_LEFT, "0"),
+    "valor_pagamento": (120, 134, PadType.ZERO_LEFT, ""), 
+    "n_doc_banco": (135, 154, PadType.SPACE_RIGHT, ""),
+    "data_real": (155, 162, PadType.ZERO_LEFT, "0"), 
+    "valor_real": (163, 177, PadType.ZERO_LEFT, "0"), 
+    "info_compl": (178, 217, PadType.SPACE_RIGHT, ""),
+    "tipo_inscricao_fav": (218, 218, PadType.ZERO_LEFT, ""), 
+    "numero_inscricao_fav_part1": (219, 226, PadType.ZERO_LEFT, "0"), 
+    "reservado_febraban_fix": (227, 229, PadType.SPACE_RIGHT, ""), 
+    "reservado_banco_fix": (230, 240, PadType.SPACE_RIGHT, "") 
+} 
 
 SEGMENTO_B = {
     "banco": (1, 3, PadType.ZERO_LEFT, "237"),
